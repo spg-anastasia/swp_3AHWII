353f17fc69aa495ca129ad5e15bd1d2992d1d364
edvingrd <Gra22087@spengergasse.at>
2024-04-16 10:23:08 +0200

    query 2 lösung

---
+++ 2024-04-02__PLF4_LSG/edvin_query.js
@@ -0,0 +1,22 @@
+console.log('here is my query:');
+// TODO
+
+const { PrismaClient } = require('@prisma/client');
+const prisma = new PrismaClient();
+
+async function getWatchlistNamesByUser(userId) {
+    const userWatchlists = await prisma.benutzer.findUnique({
+        where: { id: userId },
+        select: { Watchlist: { select: { name: true } } }
+    });
+    return userWatchlists.Watchlist.map(watchlist => watchlist.name);
+}
+
+async function getTracksFromWatchlist(watchlistId) {
+    const watchlistTracks = await prisma.watchlist.findUnique({
+        where: { id: watchlistId },
+        select: { Track: { select: { name: true, duration: true, genre: true, artist: true } } }
+    });
+    return watchlistTracks.Track;
+}
+module.exports = { getWatchlistNamesByUser, getTracksFromWatchlist };
